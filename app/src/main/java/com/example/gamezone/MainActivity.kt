c685c72fe2c0b5965878c9fe0637f6581402d3ff
AL-ALUMNO <si.bauer@duocuc.cl>
2026-09-15 16:42:18 -0300

    se agrego codigo de verificacion de formato parcial

--- app/src/main/java/com/example/gamezone/MainActivity.kt
+++ app/src/main/java/com/example/gamezone/MainActivity.kt
@@ -1,35 +1,16 @@
 package com.example.gamezone
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
-
 open class consola(val codigo: String, val marca: String, val modelo: String, val usuario: String) {
     open fun descripcion(): String {
         return "codigo: $codigo || marca: $marca || modelo: $modelo || usuario: $usuario"
     }
 
-    //open fun checkCode(): String {
-    //    if (codigo. == false) {
-    //        return "$codigo formato invalido"
-    //    }
-    //    return "$codigo"
-    //}
+    open fun checkCode(): String {
+        if (codigo.contentEquals("AA11AA") == false) {
+            return "$codigo formato invalido"
+        }
+        return "$codigo"
+    }
 }
 class consolaClasica(codigo: String, marca: String, modelo: String, usuario: String, val valor: Int = 800): consola(codigo, marca, modelo, usuario){
 
@@ -60,10 +41,10 @@ fun main(){
     val consola3 = consolaModerna("CM22TO", "Nintendo", "Switch", "infantil")
     var consola4 = consolaVR("VR44RG", "Meta", "Quest 3", "educacional", 3000, premium = true )
     var consola5 = consolaVR("VR44RG", "HTC", "Vive Pro", "infantil", 3000, premium = true )
-    val consola6 = consolaClasica("12CCCD", "Sony", "Playstation 5", "socio")
+    val consola6 = consolaClasica("CC12CD", "Sony", "Playstation 5", "socio")
 
     println(consola1.descripcion())
     println(consola4.descripcion())
     println(consola5.descripcion())
-    // println(consola6.checkCode())
+    println(consola6.checkCode())
 }
