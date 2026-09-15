18d45ee0c57d4c61949ae65b3a7524e8c6b7e125
AL-ALUMNO <si.bauer@duocuc.cl>
2026-09-15 16:57:20 -0300

    solucion de chequeo de codigo rudimetaria

--- app/src/main/java/com/example/gamezone/MainActivity.kt
+++ app/src/main/java/com/example/gamezone/MainActivity.kt
@@ -5,17 +5,18 @@ open class consola(val codigo: String, val marca: String, val modelo: String, va
         return "codigo: $codigo || marca: $marca || modelo: $modelo || usuario: $usuario"
     }
 
-    open fun checkCode(): String {
-        if (codigo.contentEquals("AA11AA") == false) {
-            return "$codigo formato invalido"
-        }
-        return "$codigo"
-    }
 }
 class consolaClasica(codigo: String, marca: String, modelo: String, usuario: String, val valor: Int = 800): consola(codigo, marca, modelo, usuario){
 
     override fun descripcion(): String {
         return super.descripcion() + "|| valor: $valor"
+    }
+
+    fun checkCode(): String {
+        if (codigo.startsWith("CC") == false) {
+            return "$codigo formato invalido"
+        }
+        return "$codigo"
     }
 }
 
@@ -23,11 +24,25 @@ class consolaModerna(codigo: String, marca: String, modelo: String, usuario: Str
     override fun descripcion(): String {
         return super.descripcion() + "|| valor: $valor"
     }
+
+    fun checkCode(): String {
+        if (codigo.startsWith("CM") == false) {
+            return "$codigo formato invalido"
+        }
+        return "$codigo"
+    }
 }
 
 class consolaVR(codigo: String, marca: String, modelo: String, usuario: String, val valor: Int = 3000, val premium: Boolean): consola(codigo, marca, modelo, usuario){
     override fun descripcion(): String {
         return super.descripcion() + "|| valor: $valor || accesorio premium: $premium"
+    }
+
+    fun checkCode(): String {
+        if (codigo.startsWith("VR") == false) {
+            return "$codigo formato invalido"
+        }
+        return "$codigo"
     }
 }
 
@@ -42,9 +57,13 @@ fun main(){
     var consola4 = consolaVR("VR44RG", "Meta", "Quest 3", "educacional", 3000, premium = true )
     var consola5 = consolaVR("VR44RG", "HTC", "Vive Pro", "infantil", 3000, premium = true )
     val consola6 = consolaClasica("CC12CD", "Sony", "Playstation 5", "socio")
+    val consola7 = consolaModerna("CC12CD", "Sony", "Playstation 5", "socio")
+    var consola8 = consolaVR("VR44RG", "Meta", "Quest 3", "educacional", 3000, premium = true )
 
     println(consola1.descripcion())
     println(consola4.descripcion())
     println(consola5.descripcion())
     println(consola6.checkCode())
+    println(consola7.checkCode())
+    println(consola8.checkCode())
 }
